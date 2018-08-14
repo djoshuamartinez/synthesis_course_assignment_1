804334e0c6fec6eb247700f2028ba49468f566d5
Joshua Martínez <djoshuamartinezpineda@gmail.com>
2018-08-13 19:32:52 -0500

    initial assignment code

---
+++ Assignment1.js
@@ -0,0 +1,167 @@
+// JavaScript source code
+
+var NUM = "NUM";
+var FALSE = "FALSE";
+var VR = "VAR";
+var PLUS = "PLUS";
+var TIMES = "TIMES";
+var LT = "LT";
+var AND = "AND";
+var NOT = "NOT";
+var ITE = "ITE";
+
+var ALLOPS = [NUM, FALSE, VR, PLUS, TIMES, LT, AND, NOT, ITE];
+
+function str(obj) { return JSON.stringify(obj); }
+
+//Constructor definitions for the different AST nodes.
+
+function flse() {
+    return { type: FALSE, toString: function () { return "false"; } };
+}
+
+function vr(name) {
+    return { type: VR, name: name, toString: function () { return this.name; } };
+}
+function num(n) {
+    return { type: NUM, val: n, toString: function () { return this.val; } };
+}
+function plus(x, y) {
+    return { type: PLUS, left: x, right: y, toString: function () { return "("+ this.left.toString() + "+" + this.right.toString()+")"; } };
+}
+function times(x, y) {
+    return { type: TIMES, left: x, right: y, toString: function () { return "(" + this.left.toString() + "*" + this.right.toString() + ")"; } };
+}
+function lt(x, y) {
+    return { type: LT, left: x, right: y, toString: function () { return "(" + this.left.toString() + "<" + this.right.toString() + ")"; } };
+}
+function and(x, y) {
+    return { type: AND, left: x, right: y, toString: function () { return "(" + this.left.toString() + "&&" + this.right.toString() + ")"; } };
+}
+function not(x) {
+    return { type: NOT, left: x, toString: function () { return "(!" + this.left.toString()+ ")"; } };
+}
+function ite(c, t, f) {
+    return { type: ITE, cond: c, tcase: t, fcase: f, toString: function () { return "(if " + this.cond.toString() + " then " + this.tcase.toString() + " else " + this.fcase.toString() + ")"; } };
+}
+
+//Interpreter for the AST.
+function interpret(exp, envt) {
+    switch (exp.type) {
+        case FALSE: return false;
+        case NUM: return exp.val;
+        case VR: return envt[exp.name];
+        case PLUS: return interpret(exp.left, envt) + interpret(exp.right, envt);
+        case TIMES: return interpret(exp.left, envt) * interpret(exp.right, envt);
+        case LT: return interpret(exp.left, envt) < interpret(exp.right, envt);
+        case AND: return interpret(exp.left, envt) && interpret(exp.right, envt);
+        case NOT: return !interpret(exp.left, envt);
+        case ITE: if (interpret(exp.cond, envt)) { return interpret(exp.tcase, envt); } else { return interpret(exp.fcase, envt); }
+    }
+}
+
+//Some functions you may find useful:
+function randInt(lb, ub) {
+    var rf = Math.random();
+    rf = rf * (ub - lb) + lb;
+    return Math.floor(rf);
+}
+
+function randElem(from) {
+    return from[randInt(0, from.length)];
+}
+
+function writeToConsole(text) {
+    var csl = document.getElementById("console");
+    if (typeof text == "string") {
+        csl.value += text + "\n";
+    } else {
+        csl.value += text.toString() + "\n";
+    }
+}
+
+
+function bottomUp(globalBnd, intOps, boolOps, vars, consts, inputoutputs) {
+    //Complete the body of randomExpr
+	return "NYI";
+}
+
+
+function bottomUpFaster(globalBnd, intOps, boolOps, vars, consts, inputoutput){
+	
+	writeToConsole("NYI");
+}
+
+
+function run1a1(){
+	
+	var rv = bottomUp(3, [VR, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [4, 5], [{x:5,y:10, _out:5},{x:8,y:3, _out:3}]);
+	writeToConsole("RESULT: " + rv.toString());
+	
+}
+
+
+function run1a2(){
+	
+	var rv = bottomUp(3, [VR, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
+		{x:10, y:7, _out:17},
+		{x:4, y:7, _out:-7},
+		{x:10, y:3, _out:13},
+		{x:1, y:-7, _out:-6},
+		{x:1, y:8, _out:-8}		
+		]);
+	writeToConsole("RESULT: " + rv.toString());
+	
+}
+
+
+function run1b(){
+	
+	var rv = bottomUpFaster(3, [VR, NUM, PLUS, TIMES, ITE], [AND, NOT, LT, FALSE], ["x", "y"], [-1, 5], [
+		{x:10, y:7, _out:17},
+		{x:4, y:7, _out:-7},
+		{x:10, y:3, _out:13},
+		{x:1, y:-7, _out:-6},
+		{x:1, y:8, _out:-8}		
+		]);
+	writeToConsole("RESULT: " + rv.toString());
+	
+}
+
+
+
+
+//Useful functions for exercise 2. 
+//Not so much starter code, though.
+
+function structured(inputoutputs){
+	return "NYI";
+}
+
+
+function run2() {
+    var inpt = JSON.parse(document.getElementById("input2").value);
+    //This is the data from which you will synthesize.
+    writeToConsole("You need to implement this");    
+}
+
+
+function genData() {
+    //If you write a block of code in program1 that writes its output to a variable out,
+    //and reads from variable x, this function will feed random inputs to that block of code
+    //and write the input/output pairs to input2.
+    program = document.getElementById("program1").value
+    function gd(x) {
+        var out;
+        eval(program);
+        return out;
+    }
+    textToIn = document.getElementById("input2");
+    textToIn.value = "[";
+    for(i=0; i<10; ++i){
+        if(i!=0){ textToIn.textContent += ", "; }
+        var inpt = randInt(0, 100);
+        textToIn.value += "[" + inpt + ", " + gd(inpt) + "]";
+    }
+    textToIn.value += "]";
+}
